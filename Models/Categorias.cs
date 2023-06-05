a0698bfabd8d538bb151c23da7f06569cb97a889
Arcangel Maldonado <nemaldonado663@gmail.com>
2023-06-05 19:43:24 -0300

    Inicializacion projecto✔

---
+++ Models/Categorias.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ElRinconDeLaCopa.Models;
+
+public class Categoria
+{
+    [Key]
+    public int ID { get; set; }
+    public string? Nombre { get; set; }
+    public virtual ICollection<Producto>? Productos { get; set; }
+}
---
+++ Models/Productos.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ElRinconDeLaCopa.Models;
+public class Producto 
+{
+    [Key]
+    public int ID { get; set; }
+    public int IDCategoria { get; set; }
+    public string? Nombre { get; set; }
+    public decimal Precio { get; set; }
+    public int Cantidad { get; set; }
+    public byte[]? Imagen { get; set; }
+    public string? NombreImagen { get; set; }
+    public string? TipoImagen { get; set; }
+    
+    [NotMapped]
+    public string? ImagenString { get; set; }
+    public virtual Categoria? Categoria { get; set; }
+}
