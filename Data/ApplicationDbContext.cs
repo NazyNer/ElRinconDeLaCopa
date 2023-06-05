cae8434e712082fb64ba9667be26f277c9a79943
Arcangel Maldonado <nemaldonado663@gmail.com>
2023-06-05 20:21:07 -0300

    👀👀Modelos, controladores y vistas 👀👀

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ElRinconDeLaCopa.Models;
 
 namespace ElRinconDeLaCopa.Data;
 
@@ -7,7 +8,7 @@ public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
-    {
-        
-    }
+    {}
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Producto>? Productos { get; set; }
 }
