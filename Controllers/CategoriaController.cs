cf53334b49fd6944cb33292505bb7edcb531a244
Arcangel Maldonado <nemaldonado663@gmail.com>
2023-08-09 21:32:57 -0300

    🔑🔑LockPage🔐🗝

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -6,11 +6,13 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 using ElRinconDeLaCopa.Data;
 using ElRinconDeLaCopa.Models;
 
 namespace ElRinconDeLaCopa.Controllers
 {
+    [Authorize]
     public class CategoriaController : Controller
     {
         private readonly ILogger<CategoriaController> _logger;
@@ -20,6 +22,8 @@ namespace ElRinconDeLaCopa.Controllers
             _context = context;
             _logger = logger;
         }
+
+        
         public IActionResult Index()
         {
             return View();
